31b1cc0435e3bba6b56346986c671b521cce57b3
S-Jed <s.jed@niceforyou.com>
2025-12-05 16:08:34 +0100

    V01

--- String.py
+++ String.py
@@ -1,3 +1,3 @@
 s="Hello"
 d="World!"
-print(s+d) 
+print(s+d)   
--- first_git.py
+++ first_git.py
@@ -1,4 +1,12 @@
-a=15
+
+from numpy import square
+
+a=15                    
 b=16
 sum=a+b
 print("The sum of",a,"and",b,"is",sum)
+Q= a*square(b)
+print(Q) 
+T=Q/b
+print(T)
+M
